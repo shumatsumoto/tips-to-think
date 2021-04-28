a09eaede7e0c970253d3e01e0e39b4093209daaa
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-28 14:37:35 +0900

    bootstrap 導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application.scss';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
