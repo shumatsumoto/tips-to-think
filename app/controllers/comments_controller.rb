aa838f62a9634774f40440e31d8bce747dfff405
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-29 19:51:08 +0900

    Comment 削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.delete
+    redirect_to comment.article, flash: { notice: 'コメントが削除されました' }
   end
 
   private
