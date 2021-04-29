1464d4410a1dfaf0ab7b822ad91bb3b71cad7063
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-29 18:50:56 +0900

    Comment createアクション作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.article
+    else
+      redirect_to :back, flash: {
+        comment: comment,
+        error_messages: comment.errors.full_messages
+      }
+    end
   end
 
   def destroy
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:article_id, :name, :comment)
+    end
 end
