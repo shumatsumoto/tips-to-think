a86f0902eb738890dc36693634b171906c62d46d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-29 12:31:26 +0900

    edit, update アクション追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+
+    redirect_to article
+  end
+
   private
     def article_params
       params.require(:article).permit(:name, :title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :new, :create, :show]
+  resources :articles, only: [:index, :new, :create, :show, :edit, :update]
 end
