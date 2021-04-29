3ff7fc824aa93d164b843d044f41bc89f63f0680
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-29 12:00:05 +0900

    詳細ページ作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     Article.create(article_params)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_params
       params.require(:article).permit(:name, :title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new'
   post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show'
 end
