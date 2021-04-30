57a7b380e471b5563c0d9f1ab2f1c60f1b7f995d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-30 12:31:39 +0900

    ユーザ新規登録機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(flash[:user])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_back fallback_location: new_user_path, flash: {
+        user: user,
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
