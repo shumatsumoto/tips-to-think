0ad3b5a37705b50fb2e1c9da2c8c4f7c30c63cdc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-04-30 12:13:01 +0900

    user 登録フォーム作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ApplicationRecord
       with: /\A[a-z0-9]+\z/,
       message: 'は小文字英数字で入力してください'
     }
-  validates: :password,
+  validates :password,
     length: { minimum: 8 }
 end
