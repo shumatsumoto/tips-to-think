8ff364ad6a2cf0b83ab6d46114ff1c9957cd8098
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-05-04 19:16:36 +0900

    Header 改修（リンク、フォント）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def header_link_item(name, path)
+  def header_link_item(name, path, options = {})
     class_name = 'nav-item'
     class_name << ' active' if current_page?(path)
 
     content_tag :li, class: class_name do
-      link_to name, path, class: 'nav-link'
+      link_to name, path, options.merge(class: 'nav-link')
     end
   end
 end
